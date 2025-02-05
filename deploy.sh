0a444c02fe61da2c7c544baf726ae18e68b3adb6
renishxxd <127740039+renishxxd@users.noreply.github.com>
2025-02-05 10:27:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u renish4804 -p 4804
+    docker login -u renish4804 -p renish40#
     docker tag test renish4804/test
     docker push renish4804/test
     
